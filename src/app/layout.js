8528184a659de04263a6740ddc7bb5386b5a09b2
Wyell Garay <23-01591@g.batstate-u.edu.ph>
2026-04-19 17:01:43 +0800

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,30 +1,19 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from '@clerk/nextjs'
 import Navbar from "./navbar";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export const metadata = {
   title: "VisionPark",
-  description: "Wyell was here.",
+  description: "-W",
 };
 
 export default function RootLayout({ children }) {
 
   return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
+    <html lang="en">
       <head>
       </head>
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body>
         <ClerkProvider
         afterSignOutUrl="/"
         signInUrl="/auth/login"
